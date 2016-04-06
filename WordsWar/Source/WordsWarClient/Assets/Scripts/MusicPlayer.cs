010cbcd4a95253cbcd93e26b6c4c8fc08ddc0568
Mauro Giusti <Maurgi@live.com>
2016-04-05 18:29:18 -0700

    Adding Unity project to WordsWarClient folder - built with Unity 5 - stupid simple three screens just to initialize the project

---
+++ WordsWar/Source/WordsWarClient/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("duplicate music player, self-destruct");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+	}
+}
